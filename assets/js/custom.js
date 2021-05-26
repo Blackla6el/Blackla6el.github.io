ce41ae795a4eece189d0cb912f450ed29886edf3
Blackla6el <56595506+Blackla6el@users.noreply.github.com>
2021-05-26 18:47:17 -0500

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -40,9 +40,10 @@
             $(function () {
                 $.vegas('slideshow', {
                     backgrounds: [
-                      { src: 'assets/img/1.jpg', fade: 1000, delay: 9000 }, //CHANGE THESE IMAGES WITH YOUR ORIGINAL IMAGES
-                      { src: 'assets/img/2.jpg', fade: 1000, delay: 9000 }, //THESE IMAGES ARE FOR DEMO PURPOSE ONLY YOU, CAN NOT USE THEM WITHOUT AUTHORS PERMISSION
-                       { src: 'assets/img/3.jpg', fade: 1000, delay: 9000 }, 
+                      //{ src: 'assets/img/1.jpg', fade: 1000, delay: 9000 }, //CHANGE THESE IMAGES WITH YOUR ORIGINAL IMAGES
+                      //{ src: 'assets/img/2.jpg', fade: 1000, delay: 9000 }, //THESE IMAGES ARE FOR DEMO PURPOSE ONLY YOU, CAN NOT USE THEM WITHOUT AUTHORS PERMISSION
+                       { src: 'assets/img/2886065.png', fade: 1000, delay: 9000 },
+                       { src: 'assets/img/2454628.png', fade: 1000, delay: 9000 }, 
                      
                     ]
                 })('overlay', {
